c6b3935d8ea96803b7aaecd9c1ffdb319a81d7c8
thokal.arun1@gmail.com <thokal.arun1@gmail.com>
2024-07-23 19:12:10 +0530

    temp class added

---
+++ git_practice/src/module1/temp1.java
@@ -0,0 +1,9 @@
+package module1;
+
+public class temp1 
+{
+public static void main(String[] args) 
+{
+	System.out.println("hello");
+}	
+}
---
+++ git_practice/src/module1/temp2.java
@@ -0,0 +1,9 @@
+package module1;
+
+public class temp2 
+{
+public static void main(String[] args) 
+{
+	System.out.println("hi");
+}
+}
